47e96a8e60074a8a16c9710e80a620e6cd4fe428
structbinary <sandeepmahto4@gmail.com>
2026-04-06 15:31:25 +0530

    deleting previous generated modules

--- vpc/endpoints.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_vpc_endpoint" "s3" {
-  vpc_id       = aws_vpc.this.id
-  service_name = "com.amazonaws.region.s3"
-  vpc_endpoint_type = "Gateway"
-  route_table_ids = concat(aws_route_table.app[*].id, aws_route_table.data[*].id)
-  tags = merge(local.common_tags, { Name = "${var.name}-s3-endpoint" })
-}
